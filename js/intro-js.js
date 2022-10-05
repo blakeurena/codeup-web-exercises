59e22f649f3a35c1f07ac0cfdb362aa8c5c49ecb
Blake Urena <Blakeurenact@gmail.com>
2022-10-05 14:43:52 -0500

    Added text to exercise and added alerts to web page.

---
+++ js/intro-js.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let greeting = "Hello from an external JS file";
+console.log(greeting);
+
+    // without "use of strict", will appear fine, WITH "use strict", appears red.
+    //  myAge = 25;
+
+ alert("You've been hacked!");
+ let userName = prompt("What is your name?");
+ // log the result of the prompt
+ console.log(userName);
+
+
+
+ let isFull = confirm("Is the class full?");
+console.log(isFull);
+
+alert("Hello " + userName + " welcome to my website!");
+
+ // let greeting = "hello from external JS";
+
+
+
