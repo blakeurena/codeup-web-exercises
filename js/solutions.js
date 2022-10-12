bb12ef764f0e46541bd21915e37650a2a02a981c
Blake Urena <Blakeurenact@gmail.com>
2022-10-12 10:05:01 -0500

    completed first    problem for practice assesment

---
+++ js/solutions.js
@@ -0,0 +1,45 @@
+
+// 1. isGreaterThan
+isGreaterThan(9.3, 7.4)            // true
+function isGreaterThan(number, number2) {
+
+}
+isGreaterThan(3.14, 3.14) {
+
+}         // false
+isGreaterThan(7.4, 9.3)  {
+
+}          // false
+isGreaterThan(-9.3, 7.4)  {
+
+}         // false
+isGreaterThan(-3.14, 3.14) {
+
+}        // false
+isGreaterThan(-7.4, 9.3)  {
+
+}         // false
+isGreaterThan(9.3, -7.4) {
+
+}          // true
+isGreaterThan(3.14, -3.14)    {
+
+}     // true
+isGreaterThan(7.4, -9.3)  {
+
+}         // true
+isGreaterThan(-9.3, -7.4)  {
+
+}        // false
+isGreaterThan(-3.14, -3.14) {
+
+}       // false
+isGreaterThan(-7.4, -9.3) {
+
+}         // true
+isGreaterThan(null, null) {
+
+}         // false
+isGreaterThan('Seven', 'eight' {
+
+}    // false
