754703c9e93f42f8f9fbcd65dc90af3534ae800a
Blake Urena <Blakeurenact@gmail.com>
2022-10-12 10:16:18 -0500

    completed problem 3in practice assesment

--- js/solutions.js
+++ js/solutions.js
@@ -43,3 +43,95 @@ isGreaterThan(null, null) {
 isGreaterThan('Seven', 'eight' {
 
 }    // false
+// 2. isLessThan
+function isLessThan(number, number2) {
+
+}
+isLessThan(9.3, 7.4)   {
+
+}         // false
+isLessThan(3.14, 3.14)    {
+
+}      // false
+isLessThan(7.4, 9.3)  {
+
+}          // true
+isLessThan(-9.3, 7.4)    {
+
+}       // true
+isLessThan(-3.14, 3.14) {
+
+}        // true
+isLessThan(-7.4, 9.3) {
+
+}          // true
+isLessThan(9.3, -7.4)   {
+
+}        // false
+isLessThan(3.14, -3.14)   {
+
+}      // false
+isLessThan(7.4, -9.3)   {
+
+}        // false
+isLessThan(-9.3, -7.4)  {
+
+}        // true
+isLessThan(3.14, -3.14)   {
+
+}     // false
+isLessThan(-7.4, -9.3)    {
+
+}      // false
+isLessThan(null, null)  {
+
+}        // false
+isLessThan('Seven', 'eight') {
+
+}   // false
+
+function isEqualNumber (number, number) {
+
+}
+isEqualNumber(9.3, 7.4)   {
+
+}         // false
+isEqualNumber(3.14, 3.14) {
+
+}         // true
+isEqualNumber(7.4, 9.3)  {
+
+}          // false
+isEqualNumber(-9.3, 7.4) {
+
+}          // false
+isEqualNumber(-3.14, 3.14) {
+
+}         // false
+isEqualNumber(-7.4, 9.3)  {
+
+}         // false
+isEqualNumber(9.3, -7.4)  {
+
+}         // false
+isEqualNumber(3.14, -3.14)  {
+
+}       // false
+isEqualNumber(7.4, -9.3) {
+
+}          // false
+isEqualNumber(-9.3, -7.4) {
+
+}         // false
+isEqualNumber(-3.14, -3.14) {
+
+}        // true
+isEqualNumber(-7.4, -9.3) {
+
+}         // false
+isEqualNumber(null, null)  {
+
+}        // false
+isEqualNumber('Seven', 'eight') {
+
+}    // false
